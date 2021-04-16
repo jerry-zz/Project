b1452b5a215b71937a46b795b1a4c824dd1831fb
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-04-16 17:30:33 +0800

    Create choose.py

---
+++ 抽奖工具/choose.py
@@ -0,0 +1,37 @@
+import random
+import time
+from tkinter import *
+
+tk = Tk()
+canvas = Canvas(tk, width=500, height=300, bg='black')
+canvas.pack()
+a = canvas.create_text(250, 100, text='预备', font=('Arial', 50), fill='yellow')
+
+
+def start():
+    filename = '内容.txt'
+    with open(file=filename, encoding='utf8') as f:
+        f = f.read()
+        try:
+            file = eval(f)
+            file = list(range(0, file))
+            file = random.choice(file)
+            canvas.itemconfig(a, text=file)
+            time.sleep(0.1)
+        except:
+            file = (f.split(','))
+            file = random.choice(file)
+            canvas.itemconfig(a, text=file)
+
+
+def quit():
+    tk.quit()
+
+
+bt_start = Button(tk, width=20, height=5, text='开始', command=start)
+bt_start.pack()
+bt_start.place(x=200, y=150)
+bt_quit = Button(tk, width=10, height=2, text='退出', command=quit)
+bt_quit.pack()
+bt_quit.place(x=400, y=250)
+tk.mainloop()
