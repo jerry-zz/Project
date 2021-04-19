e8e0a96c1f7c774083637439c1a79b9cfe5dc91d
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-04-19 22:49:43 +0800

    Update 外星入侵.py

--- 外星入侵/外星入侵.py
+++ 外星入侵/外星入侵.py
@@ -1,1 +1,14 @@
 from tkinter import *
+
+
+def ship(image, x, y):
+    ship_id = canvas.create_image(x, y, image=image, anchor='nw')
+
+
+tk = Tk()
+tk.title('外星入侵')
+tk.resizable(0, 0)
+canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
+canvas.pack()
+ship(PhotoImage(file='飞船.gif'), 250, 400)
+canvas.mainloop()
