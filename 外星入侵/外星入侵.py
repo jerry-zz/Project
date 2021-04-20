445880d53875f944c8be1ece6cfa5905ca14a4ec
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-04-20 22:11:46 +0800

    Update 外星入侵.py

--- 外星入侵/外星入侵.py
+++ 外星入侵/外星入侵.py
@@ -1,8 +1,26 @@
 from tkinter import *
 
 
-def ship(image, x, y):
-    ship_id = canvas.create_image(x, y, image=image, anchor='nw')
+class Ship:
+    def __init__(self, image, x, y, width):
+        self.width = width
+        self.image = image
+        self.id = canvas.create_image(x, y, image=self.image, anchor='nw')
+        self.x = 0
+        self.canvas_width = 500
+        canvas.bind_all('<KeyPress-Left>', self.left)
+        canvas.bind_all('<KeyPress-Right>', self.right)
+        self.pos = []
+
+    def left(self, evt):
+        self.pos = canvas.coords(self.id)
+        if self.pos[0] > 0:
+            canvas.move(self.id, -5, 0)
+
+    def right(self, evt):
+        self.pos = canvas.coords(self.id)
+        if (self.pos[0] + self.width) < self.canvas_width:
+            canvas.move(self.id, 5, 0)
 
 
 tk = Tk()
@@ -10,5 +28,11 @@ tk.title('外星入侵')
 tk.resizable(0, 0)
 canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
 canvas.pack()
-ship(PhotoImage(file='飞船.gif'), 250, 400)
+bg_photo = PhotoImage(file='背景.gif')
+ship_photo = PhotoImage(file='飞船.gif')
+biu_photo = PhotoImage(file='子弹.gif')
+spaceman_photo = PhotoImage(file='外星人.gif')
+bomb_photo = PhotoImage(file='爆炸.gif')
+canvas.create_image(0, 0, image=bg_photo, anchor='nw')
+ship = Ship(ship_photo, 250, 400, 60)
 canvas.mainloop()
