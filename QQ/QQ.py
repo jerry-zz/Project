58bb9a04f302442de9fccb5f0ba793ff3a1756ae
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-06-06 22:59:15 +0800

    QQ

--- QQ/QQ.py
+++ QQ/QQ.py
@@ -1,8 +1,11 @@
 import pyautogui as gui
 
 filename = '刷屏用语.txt'
-with open(file=filename)as word:
-    word = word.read
-for i in range(1, 500):
+with open(file=filename, encoding='utf8') as word:
+    word = word.read()
+    word=(word.split(','))
+    num=int(word[0])
+    word=word[1]
+for i in range(0, num):
     gui.typewrite(message='%s' % (word))
     gui.hotkey('enter')
