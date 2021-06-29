858a6f79e489beeea72c8f915d7b31e15e3e7f31
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-06-29 22:58:53 +0800

    ...

---
+++ 外星入侵/外星入侵(照书)/ship.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.image = pygame.image.load('image/飞船.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitime(self):
+        self.screen.bult(self.image, self.rect)
